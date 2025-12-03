4fc47712c839230a50613644d34ee96ec3b2a091
wombatwombatt <eebermas@gmail.com>
2025-12-03 21:30:15 +0800

    Delete footer.php

--- footer.php
+++
@@ -1,8 +0,0 @@
-<!--Bermas, Estella Mae E.
-    CYB-201
-    November 27, 2025 -->
-    
-<?php
-// Universal footer for all pages
-?>
-<div class="footer">Created by: Estella Mae Bermas © 2025</div>
